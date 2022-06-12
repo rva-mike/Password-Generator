b963d87bc31e3effb3b6acba6e84e6cb05f2e18b
Mike <mike.levy.rva@gmail.com>
2022-06-12 17:34:28 -0400

    created variables for character types

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
 // Assignment code here
 
 
+//Uppercase
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+
+//Lowercase
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//Numbers
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+//Special Characters
+var spCharacters = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+
+
+
+
+
+
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
